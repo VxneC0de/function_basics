afd4d1e6243a3a298bbcbad916a2ce92829ed0a0
VxneC0de <vanerubio2020@gmail.com>
2024-05-29 21:57:09 -0400

    feat: finished arrow_functions_3

--- src/arrow_functions_3.js
+++ src/arrow_functions_3.js
@@ -22,3 +22,13 @@ ask(
 
 //ANSWER:
 
+let ask = (question, yes, no) => {
+  if (confirm(question)) yes();
+  else no();
+}
+
+ask(
+  "Do you agree?",
+  () => alert("You agreed."),
+  () => alert("You canceled the execution.")
+);
